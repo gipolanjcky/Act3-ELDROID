62ca057e88c2ed4db846161582576aa05b34f647
Jaquelyn Gipolan <75476-20568905@SERVER2.DOMAIN>
2023-10-28 15:40:04 +0800

    first commit

---
+++ app/src/main/java/com/example/gipolanactivity3/DataModel.kt
@@ -0,0 +1,13 @@
+package com.example.gipolanactivity3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+data class DataModel(
+    val name: String,
+    val subject: String,
+    val body: String
+)
+
+
+
